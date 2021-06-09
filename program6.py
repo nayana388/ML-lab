62052c423f0b634a11e44f5345193d36825f7769
nayana388 <76907168+nayana388@users.noreply.github.com>
2021-06-09 13:05:46 +0530

    Add files via upload

---
+++ program6.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np, pandas as pd, matplotlib.pyplot as plt  
+
+  
+
+  
+
+  
+
+from sklearn import tree, metrics, model_selection, preprocessing   
+
+  
+
+from sklearn.neighbors import KNeighborsClassifier  
+
+  
+
+  
+
+  
+
+  
+
+  
+
+df = pd.read_csv('iris.csv')  
+
+  
+
+  
+
+  
+
+y = df.iloc[:,-1].values  
+
+x = df.iloc[:,0:4].values  
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size = 0.2)  
+
+classifier = KNeighborsClassifier(n_neighbors=3)  
+
+classifier.fit(x_train,y_train)  
+
+y_predict = classifier.predict(x_test)  
+
+acuuracy = metrics.accuracy_score(y_test, y_predict)  
+
+confusion_matrix(y_test, y_predict)  
+
+ 
+
+ 
+
+
+# In[ ]:
+
+
+
+
